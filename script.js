62aec1e342ea4bd96f34411e93c8c51d357efba0
Cordeiro0 <144747708+Cordeiro0@users.noreply.github.com>
2024-04-09 17:30:54 -0300

    Aqui os arquivos para o botão

---
+++ script.js
@@ -0,0 +1,24 @@
+var nome = document.getElementById("InputNome");
+var email = document.getElementById("InputEmail");
+var telefone = document.getElementById("InputTelefone");
+
+function Validar(){
+        if(nome == ""){
+            alert("Preencha o campo 'Nome'");
+            nome.Focus()
+            return false;
+        }
+
+        if(email == ""){
+            alert("Preencha o campo 'Email'");
+            email.Focus()
+            return false;
+        }
+
+        if(telefone == ""){
+            alert("Preencha o campo 'Telefone'");
+            telefone.Focus();
+            return false;
+        }
+}
+
