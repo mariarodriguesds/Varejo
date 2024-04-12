2c512212a4074e5b1620b35a2549d924a056012b
Cordeiro0 <144747708+Cordeiro0@users.noreply.github.com>
2024-04-12 18:14:47 -0300

    Essa é a Branch que deixa o contato responsivo
    Fiz algumas mudanças a mais

---
+++ script.js
@@ -0,0 +1,33 @@
+function Validar(){
+    var nome = document.getElementById("InputNome");
+    var email = document.getElementById("InputEmail");
+    var telefone = document.getElementById("InputTelefone");
+
+        if(nome == ""){
+            alert("Preencha o campo 'Nome'");
+            nome.Focus()
+            return false;
+        }
+
+        if(email == ""){
+            alert("Preencha o campo 'Email'");
+            email.Focus()
+            return false;
+        }
+
+        if(telefone == ""){
+            alert("Preencha o campo 'Telefone'");
+            telefone.Focus();
+            return false;
+        }
+
+        if(nome && email && telefone != ""){
+            alert("Obrigado pelo feedback!");
+
+        }
+}
+
+function Login(){
+    
+}
+
